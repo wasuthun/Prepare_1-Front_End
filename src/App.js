a0d984b0a691fd45f171a3464e667a2daa5369f1
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2019-05-23 15:44:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Graph from './components/graph';
+
+function App() {
+  return (
+    <div className="App">
+      <Graph />
+    </div>
+  );
+}
+
+export default App;
